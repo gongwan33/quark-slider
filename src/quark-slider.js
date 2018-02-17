77e0df16d0f1213da5a652e377d749a97548581a
gongwan33 <gongwan33@gmail.com>
2018-02-17 13:15:41 +0800

    first commit

---
+++ src/quark-slider.js
@@ -0,0 +1,10 @@
+(function($) {
+    $.fn.quarkSlider = function(id, options) {
+		var settings = $.extend({
+		    type: "standard",
+			scrollType: "thumbnail",
+			dotBar: "circle",
+			navArrow: "standard",
+		}, options);
+	}
+})(jQuery);
